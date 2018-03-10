5ee9e2e045cf57ec794bfe08a0abe7fb2661dc2b
DaisukeKuraishi <madgga@gmail.com>
2018-03-10 15:22:59 +0900

    オークションアプリ完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def show
-      #Itemモデルのfindメソッドを使用して、パラメータに値に一致するidのデータを検索している
-      #取得したデータを@itemという変数にセットする
-      @item = Item.find(params[:id])
-    end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  # For
-  get "/items" => "items#index"
-
-  get '/items/:id' => 'items#show'
-
-
-
+#   # For
+#   get "/items" => "items#index"
+#
+#   post "/items" => "items#create"
+#
+#   get "/items/new" => "items#new"
+#
+#   get "items/:id" => "items#show"
+#
+#   get "/items/:id/edit" => "items#edit"
+#
+#   patch "/items/:id" => "items#update"
+#
+#   delete '/items/:id' => 'items#destroy'
+#
+  resources :items
 end
